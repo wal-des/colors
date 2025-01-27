6641a1200fe5aea99f1f4b3213d990232eaafdf0
wal-des <asawasa@gmail.com>
2025-01-27 18:27:10 +0100

    Added details in modal

--- scripts/getColors.js
+++ scripts/getColors.js
@@ -47,45 +47,82 @@ async function getColorData() {
             for (var i = 0; i < allRecords.length; i++) {
 
                 document.querySelector("#color-list").innerHTML += `
-                <div class="swatch-card" id="${allRecords[i].id}">
-                    <img src=${allRecords[i].swatchImage || ''} alt="" class="color-image" onerror="this.src='../images-default/no-color-pigment.svg';">
-                    <div class="card-text">
-                        <div class="card-header-row">
-                            <h2 class="color-name">${allRecords[i].colorName || ''}</h2>
-                            <p class="color-number">${allRecords[i].colorCode || ''}</p>
+                      <li class="color-item">
+                        <div class="swatch-card" id="${allRecords[i].id}">
+                            <img src=${allRecords[i].swatchImage || ''} alt="" class="color-image" onerror="this.src='../images-default/no-color-pigment.svg';">
+                            <div class="card-text">
+                                <div class="card-header-row">
+                                    <h2 class="color-name">${allRecords[i].colorName || ''}</h2>
+                                    <p class="color-number">${allRecords[i].colorCode || ''}</p>
+                                </div>
+                                <div class="card-text-row">
+                                    <p class="make-name">${allRecords[i].brand || ''}</p>
+                                    <p class="product-line">${allRecords[i].productLine || ''}</p>
+                                </div>
+                                    <div class="card-text-row">
+                                    <p class="color-information-tag">${allRecords[i].pigments || ''}</p>
+                                </div>
+                                <div class="color-information-item obsolete" id="obsolete">
+                                    <i class="color-information-icon ${allRecords[i].obsolete || ''}"></i>
+                                    <p class="color-information-tag">${(allRecords[i].obsolete && "Not in collection anymore") || ''}</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="card-text-row">
-                            <p class="make-name">${allRecords[i].brand || ''}</p>
-                            <p class="product-line">${allRecords[i].productLine || ''}</p>
-                        </div>
-                        <div class="card-text-row">
-                            <p class="color-information-tag">${allRecords[i].pigments || ''}</p>
-                        </div>
-                        <div class="color-information-list" id="color-details">
-                            <div class="color-information-item" id="lightfastness">
-                                <i class="color-information-icon ${allRecords[i].lightfastness || ''}"></i>
-                                <p class="color-information-tag">${allRecords[i].lightfastness || ''}</p>
+                        <div class="modal">
+                            <div class="modal-container" id="colorDetails-${allRecords[i].id}">
+                            <div class="card-text">
+                                <div class="card-header-row">
+                                <h1 class="color-name">${allRecords[i].colorName || ''}</h1>
+                                <p class="color-number">${allRecords[i].colorCode || ''}</p>
+                                </div>
+                                <div class="card-text-row">
+                                <p class="make-name">${allRecords[i].brand || ''}</p>
+                                <p class="product-line">${allRecords[i].productLine || ''}</p>
+                                </div>
+                                <div class="color-information-item obsolete" id="obsolete">
+                                <i class="color-information-icon ${allRecords[i].obsolete || ''}"></i>
+                                <p class="color-information-tag">${(allRecords[i].obsolete && "Not in collection anymore")||''}</p>
+                                </div>
                             </div>
-                            <div class="color-information-item" id="opacity">
-                                <i class="color-information-icon ${allRecords[i].opacity || ''}"></i>
-                                <p class="color-information-tag">${allRecords[i].opacity || ''}</p>
-                            </div>
-                            <div class="color-information-item" id="staining">
-                                <i class="color-information-icon ${allRecords[i].staining || ''}"></i>
-                                <p class="color-information-tag">${allRecords[i].staining || ''}</p>
-                            </div>
-                            <div class="color-information-item" id="granulation">
-                                <i class="color-information-icon ${allRecords[i].granulation || ''}"></i>
-                                <p class="color-information-tag">${allRecords[i].granulation || ''}</p>
-                            </div>
+                            <div class="modal-content">
+                                <img src=${allRecords[i].swatchImage || ''} alt=""
+                                class="color-image-modal"
+                                onerror="this.src='../images-default/no-color-pigment.svg';">
 
+                                    <div class="color-information-list" id="color-details">
+                                        <div class="color-information-item" id="lightfastness">
+                                            <i class="color-information-icon ${allRecords[i].lightfastness || ''}"></i>
+                                            <p class="color-information-tag">${allRecords[i].lightfastness || ''}</p>
+                                        </div>
+                                        <div class="color-information-item" id="opacity">
+                                            <i class="color-information-icon ${allRecords[i].opacity || ''}"></i>
+                                            <p class="color-information-tag">${allRecords[i].opacity || ''}</p>
+                                        </div>
+                                        <div class="color-information-item" id="staining">
+                                            <i class="color-information-icon ${allRecords[i].staining || ''}"></i>
+                                            <p class="color-information-tag">${allRecords[i].staining || ''}</p>
+                                        </div>
+                                        <div class="color-information-item" id="granulation">
+                                            <i class="color-information-icon ${allRecords[i].granulation || ''}"></i>
+                                            <p class="color-information-tag">${allRecords[i].granulation || ''}</p>
+                                        </div>
+                                    </div>
+
+                                <div class="details-section">
+                                    <h2>Pigments</h2>
+                                    <div class="card-text-row">
+                                    <p class="color-information-tag">${allRecords[i].pigments || 'No pigment information.'}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="button-container">
+                                <button type="button" class="btn-primary" onclick="closeDetails()">
+                                Stäng
+                                </button>
+                            </div>
+                            </div>
                         </div>
-                        <div class="color-information-item obsolete" id="obsolete">
-                            <i class="color-information-icon ${allRecords[i].obsolete || ''}"></i>
-                            <p class="color-information-tag">${(allRecords[i].obsolete && "Not in collection anymore")|| ''}</p>
-                        </div>
-                    </div>
-                </div>
+                    </li>
                 `;
 
 
--- scripts/openDetails.js
+++ scripts/openDetails.js
@@ -1,18 +1,34 @@
         //Show details modal
 
         // Get the modal
-        var modal = document.getElementById("colorDetails");
+        // var modal = document.getElementById("colorDetails");
 
-        function openDetails() {
-            document.getElementById("colorDetails").style.display = "block";
-        }
+        // function openDetails() {
+        //     document.getElementById("colorDetails").style.display = "block";
+        // }
 
-        function closeDetails() {
-            document.getElementById("colorDetails").style.display = "none";
-        }
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
+        // function closeDetails() {
+        //     document.getElementById("colorDetails").style.display = "none";
+        // }
+        // // When the user clicks anywhere outside of the modal, close it
+        // window.onclick = function (event) {
+        //     if (event.target == modal) {
+        //         modal.style.display = "none";
+        //     }
+        // }
+
+
+        $(document).on("click", ".swatch-card", function () { 
+            console.log("Clicked " + this.id);
+        
+            $(".modal").removeClass("modalOpen");
+            $(this).siblings('.modal').addClass("modalOpen");
+            $('body').css('overflow', 'hidden');
+          
+          });
+        
+          function closeDetails() {
+            console.log("Clicked close button");
+            $(".modal").removeClass("modalOpen");
+            $('body').css('overflow', 'auto');
+          }
